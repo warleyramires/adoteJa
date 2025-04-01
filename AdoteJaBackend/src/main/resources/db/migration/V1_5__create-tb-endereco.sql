ce32f9600cac3e71a96b2da2cd6689f499b0b4eb
Warley Ramires <warleyramiresgoncalves@gmail.com>
2025-04-01 16:54:20 -0300

    ✨ feat: Create Migration tb_endereco

---
+++ AdoteJaBackend/src/main/resources/db/migration/V1_5__create-tb-endereco.sql
@@ -0,0 +1,9 @@
+CREATE TABLE tb_endereco (
+    id SERIAL PRIMARY KEY,
+    logadouro VARCHAR(255) NOT NULL,
+    numero VARCHAR(50),
+    bairro VARCHAR(100),
+    cidade VARCHAR(100) NOT NULL,
+    estado VARCHAR(50) NOT NULL,
+    cep VARCHAR(20) NOT NULL
+);
