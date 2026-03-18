bdbdc31f6eb376336dfa421b4aa9483760c9efef
Warley Ramires <warleyramiresgoncalves@gmail.com>
2026-03-18 16:21:39 -0300

    feat: adicionar página de cadastro de adotante

--- AdoteJaFrontend/src/router/index.tsx
+++ AdoteJaFrontend/src/router/index.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { HomePage } from '../pages/HomePage'
 import { LoginPage } from '../pages/LoginPage'
+import { CadastroPage } from '../pages/CadastroPage'
 import { PetsPage } from '../pages/PetsPage'
 import { NotFoundPage } from '../pages/NotFoundPage'
 
 export const router = createBrowserRouter([
-  { path: '/',       element: <HomePage /> },
-  { path: '/login',  element: <LoginPage /> },
-  { path: '/pets',   element: <PetsPage /> },
-  { path: '*',       element: <NotFoundPage /> },
+  { path: '/',         element: <HomePage /> },
+  { path: '/login',    element: <LoginPage /> },
+  { path: '/cadastro', element: <CadastroPage /> },
+  { path: '/pets',     element: <PetsPage /> },
+  { path: '*',         element: <NotFoundPage /> },
 ])
